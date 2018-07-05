e66b703941dc7ed99cbd507bc75656cc2d1ff7f6
Muhammad Habib Rohman <mhrohman@live.com>
2018-07-05 19:58:47 +0700

    Fix untracted files

--- packages/constants/dist/ui.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const PRIMARY_COLOR = "#d62829";
-export declare const SECONDARY_COLOR = "red";
-export declare const GREY_COLOR = "#dddddd";
--- packages/constants/src/ui.ts
+++
@@ -1,3 +0,0 @@
-export const PRIMARY_COLOR = '#d62829'
-export const SECONDARY_COLOR = 'red'
-export const GREY_COLOR = '#dddddd'
